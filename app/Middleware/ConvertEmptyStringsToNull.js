dbff651a4e70c460ed8eb09997b53f33dfab3ae6
Mike Conrad <mike@enxo.co>
2020-01-26 18:46:36 -0500

    Refactored to use Redis built in Incr function to handle hit counter

--- app/Middleware/ConvertEmptyStringsToNull.js
+++ app/Middleware/ConvertEmptyStringsToNull.js
@@ -4,23 +4,11 @@ const Redis = use('Redis')
 class ConvertEmptyStringsToNull {
   async handle ({ request }, next) {
     const { tool } = request.all()
-    let hits = await Redis.get("hits")
     if(tool){
-      let toolHits = await Redis.get(tool)
-      if(toolHits){
-        Number(toolHits++)
-        Redis.set(tool, toolHits)
-        request.secrets = toolHits
-      } else {
-        Redis.set(tool, 1)
-      }
+      Redis.incr(tool)
     }
-    if (hits) {
-      Number(hits++)
-      Redis.set("hits", hits)
-    } else {
-      Redis.set("hits", 1)
-    }
+
+    Redis.incr('hits')
     if (Object.keys(request.body).length) {
       request.body = Object.assign(
         ...Object.keys(request.body).map(key => ({
